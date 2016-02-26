5e952fd498d4dd68da3a155f8c997c58ae9eea62
NotTheEvilOne <git-NTEO@vplace.de>
2016-02-26 10:26:31 +0100

    Enhancement: Added "DatabaseTable" to hold a database instance while rendering table rows

---
+++ src/dNG/pas/module/controller/output/database_table.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+##j## BOF
+
+"""
+direct PAS
+Python Application Services
+----------------------------------------------------------------------------
+(C) direct Netware Group - All rights reserved
+https://www.direct-netware.de/redirect?pas;http;table
+
+This Source Code Form is subject to the terms of the Mozilla Public License,
+v. 2.0. If a copy of the MPL was not distributed with this file, You can
+obtain one at http://mozilla.org/MPL/2.0/.
+----------------------------------------------------------------------------
+https://www.direct-netware.de/redirect?licenses;mpl2
+----------------------------------------------------------------------------
+#echo(pasHttpTableVersion)#
+#echo(__FILEPATH__)#
+"""
+
+from dNG.pas.database.connection import Connection
+from .table import Table
+
+class DatabaseTable(Table):
+#
+	"""
+The "DatabaseTable" is used to render a table of rows of columns from the
+database.
+
+:author:     direct Netware Group
+:copyright:  (C) direct Netware Group - All rights reserved
+:package:    pas.http
+:subpackage: table
+:since:      v0.1.02
+:license:    https://www.direct-netware.de/redirect?licenses;mpl2
+             Mozilla Public License, v. 2.0
+	"""
+
+	@Connection.wrap_callable
+	def execute_render(self):
+	#
+		"""
+Action for "render"
+
+:since: v0.1.02
+		"""
+
+		Table.execute_render(self)
+	#
+#
+
+##j## EOF
